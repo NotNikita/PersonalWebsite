f5da53d3f08f40262e253df10199464ecd0277fc
Nikita <47776039+NotNikita@users.noreply.github.com>
2024-10-19 17:31:38 +0200

    Update about.tsx

--- src/content/about.tsx
+++ src/content/about.tsx
@@ -1,5 +1,10 @@
 export const present: string[] = [
   "I am a 24 years old from *Poland* who loves technology, coding, architecture and volleyboll!",
 
-  "Currently, I am working on my own projects while learing new technologies and improving my skills in FullStack development.",
+  "Fullstack Engineer with over 3 years of experience in agile environments, specializing in building, improving,
+  and maintaining scalable web and mobile applications. Proficient in JavaScript, React and Next.js 
+  for frontend development. On the backend, experienced in creating REST APIs and scalable
+  services using Node.js, and Python, with strong expertise in MySQL, Postgres and NoSQL databases. Possess
+  excellent technical and non-technical communication skills and effectively managing
+  schedules. A strong team player and contributor with advanced English proficiency",
 ];
