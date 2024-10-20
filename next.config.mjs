ba686cb2f3be59f70e20e9534a8ec13cdb45c87d
NotNikita <nikitabsuir@gmail.com>
2024-10-21 01:05:47 +0200

    adding slash to assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: "/PersonalWebsite",
+  assetPrefix: "/PersonalWebsite/",
   images: {
     domains: ["https://notnikita.github.io/PersonalWebsite/", "http://www.nik-yaskevich.xyz/"],
   },
